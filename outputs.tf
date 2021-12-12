acd70957c6f147c10803140c4c1c9e1cd2bd732a
meela <meelapradhan400@gmail.com>
2021-12-12 14:23:39 -0800

    added commands in terranform outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,16 @@
-output "abc" {
-  value = "Hello\nWorld"
+#output "abc" {
+#  value = "Hello\nWorld"
+#}
+#
+#output "abc1" {
+#  value = "100"
+#}
+
+variable "abc" {
+  default = "100"
 }
+output "abc" {
+  value = var.abc
+}
+
+
