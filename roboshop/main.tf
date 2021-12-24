608fd261eff8e5f438decfa843f255e7037f3d9a
meela <meelapradhan400@gmail.com>
2021-12-24 12:59:58 -0800

    fixed bug

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -5,7 +5,7 @@ resource "aws_spot_instance_request" "cheap_worker" {
   vpc_security_group_ids = ["sg-06ec2f14a6137cc44"]
   wait_for_fulfillment = true
   tags = {
-    Name = element(var.components, count.index)
+    Name = local.COMP_NAME
   }
 }
 
@@ -13,7 +13,24 @@ resource "aws_ec2_tag" "tags" {
   count       = length(var.components)
   resource_id = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
   key         = "Name"
-  value       = element(var.components, count.index)
+  value       = local.COMP_NAME
+}
+
+resource "null_resource" "ansible" {
+  count = length(var.components)
+  provisioner "remote-exec" {
+    connection {
+      host     = element(aws_spot_instance_request.cheap_worker.*.private_ip, count.index)
+      user     = "centos"
+      password = "DevOps321"
+    }
+    inline = [
+      "yum install python3-pip -y",
+      "pip3 install pip --upgrade",
+      "pip3 install ansible",
+      "ansible-pull -U https://github.com/pradhan02/ansible.git -e COMPONENT=${local.COMP_NAME}"
+    ]
+  }
 }
 
 data "aws_ami" "ami" {
@@ -28,5 +45,8 @@ variable "components" {
 
 provider "aws" {
   region = "us-east-1"
+}
+locals{
+  COMP_NAME = element(var.components, count.index)
 }
 
