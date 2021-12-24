f26bda95a2060b379d1c217979728930f4bf0c72
meela <meelapradhan400@gmail.com>
2021-12-24 11:57:32 -0800

    added commands in roboshop main.tf

---
+++ roboshop/main.tf
@@ -0,0 +1,19 @@
+resource "aws_spot_instance_request" "cheap_worker" {
+  count         = length(var.components)
+  ami           = data.aws_ami.ami.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-06ec2f14a6137cc44"]
+  tags = {
+    Name = element(var.components,count.index)
+  }
+}
+
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "^Cent*"
+  owners      = ["973714476881"]
+}
+
+variable "components" {
+  default = ["frontend", "mongodb", "catalogue", "cart", "user", "redis", "mysql", "shipping", "rabbitmq", "payment"]
+}
