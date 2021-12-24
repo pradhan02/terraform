d4d482ebf5552276aabc83a139f2d8e1c61be709
meela <meelapradhan400@gmail.com>
2021-12-24 11:25:06 -0800

    added commands in examples main.tf

---
+++ examples/ec2/main.tf
@@ -0,0 +1,43 @@
+resource "aws_instance" "sample" {
+  count                  = var.env == "prod" ? 1 : 0
+  ami                    = data.aws_ami.example.id
+  instance_type          = var.instance_type == "" ? "t3.micro" : var.instance_type
+  vpc_security_group_ids = [var.SGID]
+
+  tags = {
+    Name = local.NAME
+  }
+}
+
+resource "null_resource" "sample" {
+  #  triggers = {
+  #    abc = aws_instance.sample.*.private_ip
+  #  }
+  provisioner "remote-exec" {
+    connection {
+      host     = aws_instance.sample.*.public_ip[0]
+      user     = "ubuntu"
+      password = "DevOps321"
+    }
+
+    inline = [
+      "echo Hello"
+    ]
+  }
+}
+
+variable "SGID" {}
+variable "name" {}
+
+variable "instance_type" {}
+variable "env" {}
+
+data "aws_ami" "example" {
+  most_recent = true
+  name_regex  = "^Ubuntu*"
+  owners      = ["973714476881"]
+}
+
+locals {
+  NAME = "${var.env}-instance"
+}
--- examples/main.tf
+++ examples/main.tf
@@ -2,57 +2,25 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "sample" {
-  ami                    = "ami-0855cab4944392d0a"
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_sample.id, "sg-06ec2f14a6137cc44"]
-
-  tags = {
-    Name = "sample"
-  }
-}
-
-resource "aws_security_group" "allow_sample" {
-  name        = "allow_sample"
-  description = "Allow sample traffic"
-
-  ingress = [
-    {
-      description      = "TLS from VPC"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-    }
-  ]
-
-  egress = [
-    {
-      description      = "egress"
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-    }
-  ]
-
-  tags = {
-    Name = "allow_sample"
-  }
-}
-
 terraform {
   backend "s3" {
-    bucket = "terraform-b60mpradhan"
+    bucket = "terraform-b60"
     key    = "example/sample/terraform.tfstate"
     region = "us-east-1"
   }
 }
+
+module "ec2" {
+  source        = "./ec2"
+  SGID          = module.sg.SGID
+  name          = ["new1", "new2"]
+  instance_type = var.instance_type
+  env           = var.env
+}
+
+module "sg" {
+  source = "./sg"
+}
+
+variable "instance_type" {}
+variable "env" {}
